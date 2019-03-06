e87624c5449e297bc7de90ec81f446eb82435a3d
Julian Baehr <julian.baehr@googlemail.com>
2019-03-06 20:47:10 +0100

    added add local and param methods

--- src/com/github/The127/MScript/models/impl/FunctionModel.java
+++ src/com/github/The127/MScript/models/impl/FunctionModel.java
@@ -4,13 +4,20 @@
  */
 package com.github.The127.MScript.models.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.github.The127.MScript.FileContext;
+import com.github.The127.MScript.MScriptCompilationException;
 import com.github.The127.MScript.models.IFunctionContext;
 import com.github.The127.MScript.models.IScriptContext;
 
 public class FunctionModel extends AbstractModel implements IFunctionContext {
 
 	private final String name;
+	
+	private Map<String, String> locals = new HashMap<>();
+	private Map<String, String> params = new HashMap<>();
 	
 	public FunctionModel(FileContext ctx, String name) {
 		super(ctx);
@@ -19,6 +26,22 @@ public class FunctionModel extends AbstractModel implements IFunctionContext {
 	
 	public String getName() {
 		return name;
+	}
+	
+	public void addLocal(String local, FileContext ctx) {
+		if(!isUniqueName(local))
+			throw new MScriptCompilationException("Duplicate variable and/or param name.", ctx);
+		//TODO
+	}
+	
+	public void addParam(String param, FileContext ctx) {
+		if(!isUniqueName(param))
+			throw new MScriptCompilationException("Duplicate param and/or variable name.", ctx);
+		//TODO
+	}
+	
+	private boolean isUniqueName(String name) {
+		return locals.containsKey(name) || params.containsKey(name);
 	}
 
 	@Override
