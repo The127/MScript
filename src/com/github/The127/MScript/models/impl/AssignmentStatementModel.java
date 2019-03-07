ed3388f42e07f7a11000d3e993272c5c85a24a48
Julian Baehr <julian.baehr@googlemail.com>
2019-03-07 19:44:51 +0100

    added write statement

--- src/com/github/The127/MScript/models/impl/AssignmentStatementModel.java
+++ src/com/github/The127/MScript/models/impl/AssignmentStatementModel.java
@@ -5,8 +5,21 @@ import com.github.The127.MScript.models.IScriptContext;
 
 public class AssignmentStatementModel extends StatementModel  {
 
-	public AssignmentStatementModel(FileContext ctx) {
+	private final String variable;
+	
+	private ExpressionModel expression;
+	
+	public AssignmentStatementModel(FileContext ctx, String variable) {
 		super(ctx);
+		this.variable = variable;
+	}
+	
+	public String getVariable() {
+		return variable;
+	}
+	
+	public void setExpression(ExpressionModel expression) {
+		this.expression = expression;
 	}
 
 	@Override
