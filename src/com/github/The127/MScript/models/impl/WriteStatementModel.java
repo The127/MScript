ac151f8c8bf0f7a00a1de82fc80a491b48329a7a
Julian Baehr <julian.baehr@googlemail.com>
2019-03-07 19:19:46 +0100

    added writestmtModel

--- src/com/github/The127/MScript/models/impl/WriteStatementModel.java
+++ src/com/github/The127/MScript/models/impl/WriteStatementModel.java
@@ -5,14 +5,33 @@ import com.github.The127.MScript.models.IScriptContext;
 
 public class WriteStatementModel extends StatementModel {
 
-	public WriteStatementModel(FileContext ctx) {
+	private final String device, target, variable;
+	
+	public WriteStatementModel(FileContext ctx, String device, String target, String variable) {
 		super(ctx);
+		this.device = device;
+		this.target = target;
+		this.variable = variable;
+	}
+	
+	public String getDevice() {
+		return device;
+	}
+	
+	public String getTarget() {
+		return target;
+	}
+	
+	public String getVariable() {
+		return variable;
 	}
 
 	@Override
 	public String compile(IScriptContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		return "s"
+				+ " " + ctx.resolveAlias(getDevice(), getFileContext())
+				+ " " + getTarget()
+				+ " " + ctx.resolveRegister(getVariable(), getFileContext()) ;
 	}
 
 }
