1b01449e4566aeae43f69a431c42b7d5d8ef6b61
Julian Baehr <julian.baehr@googlemail.com>
2019-03-08 20:39:14 +0100

    started implementing condition, elif, else

--- src/com/github/The127/MScript/models/impl/ElifStatementModel.java
+++ src/com/github/The127/MScript/models/impl/ElifStatementModel.java
@@ -1,5 +1,31 @@
 package com.github.The127.MScript.models.impl;
 
-public class ElifStatementModel {
+import com.github.The127.MScript.FileContext;
+import com.github.The127.MScript.models.IScriptContext;
 
+public class ElifStatementModel extends AbstractModel {
+
+	private ExpressionModel condition;
+	private BlockModel block;
+
+	public ElifStatementModel(FileContext ctx) {
+		super(ctx);
+	}
+	
+	public void setCondition(ExpressionModel condition) {
+		this.condition = condition;
+	}
+	
+	public void setBlock(BlockModel block) {
+		this.block = block;
+	}
+
+	@Override
+	public String compile(IScriptContext ctx) {
+		var sb = new StringBuilder();
+		
+		sb.append(block.compile(ctx));
+		
+		return sb.toString();
+	}
 }
--- src/com/github/The127/MScript/models/impl/ElseStatementModel.java
+++ src/com/github/The127/MScript/models/impl/ElseStatementModel.java
@@ -1,5 +1,27 @@
 package com.github.The127.MScript.models.impl;
 
-public class ElseStatementModel {
+import com.github.The127.MScript.FileContext;
+import com.github.The127.MScript.models.IScriptContext;
+
+public class ElseStatementModel extends AbstractModel {
+	
+	private BlockModel block;
+
+	public ElseStatementModel(FileContext ctx) {
+		super(ctx);
+	}
+	
+	public void setBlock(BlockModel block) {
+		this.block = block;
+	}
+
+	@Override
+	public String compile(IScriptContext ctx) {
+		var sb = new StringBuilder();
+		
+		sb.append(block.compile(ctx));
+		
+		return sb.toString();
+	}
 
 }
