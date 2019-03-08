3ae293489bef8d77520d34a7e6c81e499f65a130
Julian Baehr <julian.baehr@googlemail.com>
2019-03-08 20:20:36 +0100

    added fn call stmt

---
+++ src/com/github/The127/MScript/models/impl/FunctionCallModel.java
@@ -0,0 +1,11 @@
+package com.github.The127.MScript.models.impl;
+
+import com.github.The127.MScript.FileContext;
+
+public class FunctionCallModel extends ExpressionModel {
+
+	public FunctionCallModel(FileContext ctx) {
+		super(ctx);
+	}
+
+}
--- src/com/github/The127/MScript/models/impl/FunctionCallStatementModel.java
+++ src/com/github/The127/MScript/models/impl/FunctionCallStatementModel.java
@@ -5,14 +5,21 @@ import com.github.The127.MScript.models.IScriptContext;
 
 public class FunctionCallStatementModel extends StatementModel  {
 
+	private FunctionCallModel functionCall;
+	
 	public FunctionCallStatementModel(FileContext ctx) {
 		super(ctx);
 	}
 
+	public void setFunctionCall(FunctionCallModel functionCall) {
+		this.functionCall = functionCall;
+	}
+	
 	@Override
 	public String compile(IScriptContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		return functionCall.compile(ctx)
+				// dont care for result in fn call stmt
+				+ "pop r12" + System.lineSeparator();
 	}
 
 }
